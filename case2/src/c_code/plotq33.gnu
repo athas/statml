d74d2aa22e803d8e71b3e0b81d392705c8aff298
Daniel Fairchild <daniel.fairchild@gmail.com>
2011-03-17 17:30:52 +0100

    done, men vil gerne pudse lidt

--- case2/src/c_code/plotq33.gnu
+++ case2/src/c_code/plotq33.gnu
@@ -1,19 +1,19 @@
 #set terminal epslatex solid color colortext  size 15cm,15cm
-set terminal pdf solid color size  15cm, 10cm
+set terminal pdf solid color 
+#size  15cm, 10cm
 
 ddiv(x,y) = x/y
 
 
-set xrange [1:1000]
-set yrange [0:36]
+set xrange [0:5000]
+set yrange [0:100]
 
 set output 'fig_out/q33.pdf'
 
-plot 'fig_out/q33.dat' using 1:2 with lines lt 1 title 'mean length',\
-plot '' using 1:3 with lines lt 2 title 'mean dist',\
-plot '' using 1:(ddiv($3,$2)) with lines lt 3 title 'length/dist'
+plot 'fig_out/q33.dat' using 1:2 with lines lt 1 title 'avg. length',\
+ '' using 1:3 with lines lt 2 title 'avg. dist'
 
 set output 'fig_out/q33dol.pdf'
-set xrange [0:40]
-set yrange [0:25]
-plot 'fig_out/q33.dat' using 2:3 with lines lt 4 title 'mean of dist'
+set xrange [0:80]
+set yrange [0:60]
+plot 'fig_out/q33.dat' using 2:3 with lines lt 4 title '(avg. length)/(avg. dist)'
--- case2/src/c_code/question32.c
+++ case2/src/c_code/question32.c
@@ -21,7 +21,7 @@ static double other_dist(vect* x, vect* z, int lim){
 int main(int argv, char** argc){
 	printf("\n\nRunning code for question 3.2:\n\n");
 
-  printf("running set C\n");
+  printf("running set C\n");  
 	mtrx* train_set = read_knoll(DATA_PATH"knollC-train.dt"),
       * test_set  = read_knoll(DATA_PATH"knollC-test.dt");
     
@@ -29,6 +29,20 @@ int main(int argv, char** argc){
     double RMS = knn_knoll_hitrate(train_set, test_set, ik, other_dist);
     printf("K = %d hitrate == %.1f%%\n", ik, RMS*100);
   }
+  
+//  printf("Example of Mx:\n");
+  gsl_matrix_view Mv = gsl_matrix_view_array(M_data, 2, 2);
+  gsl_vector_view x_v = gsl_vector_view_array(test_set->data, 2);
+  double wrk[2];
+  gsl_vector_view tmp = gsl_vector_view_array(wrk, 2);
+  gsl_blas_dgemv(CblasNoTrans, 1, &Mv.matrix, &x_v.vector, 0, &tmp.vector);
+  
+  FILE* fp = fopen(FIG_OUT"q32mnv.tex", "w");
+  mtrx2tex(&Mv.matrix, "q32mtrxM",fp);
+  vect2tex(&x_v.vector, "q32vectX", fp);
+  vect2tex(&tmp.vector, "q32vectRes", fp);
+  fclose(fp);
+  
   gsl_matrix_free(train_set);
   gsl_matrix_free(test_set);
 }
